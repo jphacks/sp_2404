3fde131d2d889b9e6fbbc688e756ee4967d12678
s4d-towa-tsunoka <142939193+TTTowa@users.noreply.github.com>
2024-10-27 10:59:24 +0900

    ログアウト機能追加

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 # users/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.user_list, name='user_list'),  # ユーザー一覧
     # path('<int:user_id>/', views.user_detail, name='user_detail'),  # ユーザー詳細
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('<str:username>/', views.user_profile, name='user_profile'),
+    path('profile/<str:username>/', views.user_profile, name='user_profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
