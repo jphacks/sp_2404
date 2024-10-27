ab23769fd1198bbdb804970a6e4102bfaa76588a
s4d-towa-tsunoka <142939193+TTTowa@users.noreply.github.com>
2024-10-27 10:46:20 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,15 @@ from users.views import UserViewSet, user_list
 from posts.views import PostViewSet, post_list
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.shortcuts import redirect
 
 router = routers.DefaultRouter()
 # router.register(r'users', UserViewSet)
 router.register(r'posts', PostViewSet)
+
+def redirect_to_home(request):
+    # 任意のURLにリダイレクト（ここでは 'users/' にリダイレクト）
+    return redirect('posts/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +22,5 @@ urlpatterns = [
     path('api/allauth/', include('allauth.urls')),  # ソーシャル認証API
     path('users/', include('users.urls')),  # ユーザーのURL
     path('posts/', include('posts.urls')),  # 投稿のURL
+    path('', redirect_to_home),  # 空のパスにアクセスした場合のリダイレクト
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
